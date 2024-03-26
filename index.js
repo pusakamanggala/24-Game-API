9565b594f871eecc92508711a34d71a1fc9a0a78
Pusaka Manggala <pusakamanggala@gmail.com>
2024-03-26 19:21:01 +0700

    add cors

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors middleware
+
 const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const appRoute = require('./src/routes/routes');
-app.use('/', appRoute);
+// Use cors middleware to handle CORS
+app.use(cors());
+
+const appRoute = require("./src/routes/routes");
+app.use("/", appRoute);
 
 const PORT = process.env.PORT || 8080;
 
